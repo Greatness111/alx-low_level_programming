98fa1e210f7b924523f122709cbc0691c3f8ac99
greatness111 <oketachinedu@gmail.com>
2022-09-15 13:20:16 -0700

    nested loop

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,36 +1,23 @@
-#ifndef MAIN_H_INCLUDED
+#ifndef MAIN_H
+#define MAIN_H
 
-#define MAIN_H_INCLUDED
+/**
+ * main - header file for protoypes
+ * void protoypes (void)
+ * int protoypes (int)
+ */
 
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 
-
-int _putchar(char c);
-
-void print_alphabet(void);
-
-void print_alphabet_x10(void);
-
-int _islower(int c);
-
-int _isalpha(int c);
-
-int print_sign(int n);
-
-int _abs(int n);
-
-int print_last_digit(int n);
-
-void jack_bauer(void);
-
-void times_table(void);
-
-int add(int a, int b);
-
-void print_to_98(int n);
-
-void print_times_table(int n);
-
-
-
-#endif
-
+#endif /* MAIN_H */
